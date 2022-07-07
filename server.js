443fde4d01985cc926c65207ff90d316e89902f3
Muhammad Aman Yasin <M.AMANYASIN@GMAIL.COM>
2022-07-07 22:29:23 +0200

    heroku updates

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static('./dist/than-os'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, './../dist/than-os/index.html'));
+    console.log('path', path.join(_dirname, './. ./dist/than-os/index.html'))
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
