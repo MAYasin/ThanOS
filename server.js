003c6baa30d92a54c1065c843faf484b0bd8df61
Muhammad Aman Yasin <M.AMANYASIN@GMAIL.COM>
2022-07-08 14:55:30 +0200

    trying helmet

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
+const helmet = require('helmet');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-
-app.use(allowCrossDomain());
+app.use(helmet());
 
 app.use(express.static('./dist/than-os'));
 app.get('/*', function (req, res) {
